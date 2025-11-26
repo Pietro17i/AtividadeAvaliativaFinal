c7b14dfb374c4a40f989215ff3c293b0a0a644a9
Pietro17i <pixpietro01@gmail.com>
2025-11-26 17:22:44 -0300

    Create ex8.js

---
+++ ex8.js
@@ -0,0 +1,7 @@
+let pessoa1 = {"nome":"ana", "idade": 14};
+let pessoa2 = {"nome":"leo", "idade": 18};
+let pessoa3 = {"nome":"nina","idade": 13};
+
+if (pessoa1.idade >= 18) console.log(pessoa1.nome);
+if (pessoa2.idade >= 18) console.log(pessoa2.nome);
+if (pessoa3.idade >=18) console.log(pessoa3.nome);
