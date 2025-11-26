93f2fc9893c8928e17f50d1dc832c968614629e3
Pietro17i <pixpietro01@gmail.com>
2025-11-26 17:22:09 -0300

    Add name search in array example

---
+++ ex6.js
@@ -0,0 +1,11 @@
+let nomes = ["Joao", "Larissa", "Ana", "Carlos"];
+let achei = false;
+
+if (nomes[0] === "Ana") achei = true;
+if (nomes[1] === "Ana") achei = true;
+if (nomes[2] === "Ana") achei = true;
+if (nomes[3] === "Ana") achei = true;
+
+if (achei) console.log("O nome ana existe no array");
+
+
