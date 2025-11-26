a116b963c04eb26884d6c8ab98762520d688e12e
Pietro17i <pixpietro01@gmail.com>
2025-11-26 17:20:51 -0300

    Create ex2.js

---
+++ ex2.js
@@ -0,0 +1,11 @@
+let valores = [5, 12, 3, 18, 7];
+
+let cont = 0;
+
+if (valores[0] > 10) cont = +1;
+if (valores[1] > 10) cont = +1;
+if (valores[2] > 10) cont = +1;
+if (valores[3] > 10) cont = +1;
+if (valores[4] > 10) cont = +1;
+
+console.log(cont);
