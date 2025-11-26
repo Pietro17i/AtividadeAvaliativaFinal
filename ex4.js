745fe2329019fe7efaf62fbe6a44e5444ff193b7
Pietro17i <pixpietro01@gmail.com>
2025-11-26 17:21:33 -0300

    Create ex4.js

---
+++ ex4.js
@@ -0,0 +1,11 @@
+let numeros = [10, 50, 22, 3, 7];
+
+let maior = numeros[0];
+
+if (numeros[1] > maior) maior = numeros[1];
+if (numeros[2] > maior) maior = numeros[2];
+if (numeros[3] > maior) maior = numeros[3];
+if (numeros[4] > maior) maior = numeros[4];
+
+console.log(maior);
+
