5b20e03eca1f206d668e224b1493db78726d8ea2
Pietro17i <pixpietro01@gmail.com>
2025-11-26 17:21:44 -0300

    Add student grade calculation and approval check

---
+++ ex5.js
@@ -0,0 +1,11 @@
+let notas = [8, 6, 7];
+
+let soma = notas[0] + notas[1] + notas[2];
+
+let media = soma / 3;
+
+if (media > 6) {
+    console.log("Media:", media, "Aluno Aprovado");
+}else {
+     console.log("Media:", media, "Aluno Reprovado");
+}
