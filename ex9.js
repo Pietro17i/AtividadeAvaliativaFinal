83a49c85c971cf8f24ac329f9fd9d7d623d31d34
Pietro17i <pixpietro01@gmail.com>
2025-11-26 17:23:09 -0300

    Add product objects and price comparison logic

---
+++ ex9.js
@@ -0,0 +1,7 @@
+let produto1 = {"produto":"lapis", "preço":2};
+ let produto2 = {"produto":"mochila","preço":80}
+  let produto3 = {"produto":"caderno","preço":15}
+
+if (produto1.preço < 20) console.log(produto1.produto);
+if (produto2.preço <20) console.log(produto2.produto);
+if (produto3.preço <20) console.log(produto3.produto);
